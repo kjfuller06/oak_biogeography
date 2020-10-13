e3e7e1715d8c0a7cf1a5cf412aaf398ef250aef2
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-10-13 16:58:02 -0500

    Was working on Q. margarettae when I noticed a whole BUNCH of spot-on county centroids from the BISON database that made it past CoordinateCleaner

--- scripts/BONAP_county_level_check.R
+++ scripts/BONAP_county_level_check.R
@@ -9,7 +9,7 @@ library(tmaptools)
 counties <- st_read("data/county boundaries/acs_2012_2016_county_us_B27001.shp") %>% 
   dplyr::select(NAME,
                 geometry)
-records = read.csv("outputs/cleaned_GBIF_BISON_recordsV.1.csv")
+records = read.csv("outputs/all_recordsV.1.csv")
 records = records %>% 
   st_as_sf(coords = c(lon = "lon", lat = "lat"), crs = 4326)
 
@@ -26,3 +26,13 @@ tm_shape(counties)+
   tm_shape(records[records$species == "Quercus geminata",])+
   tm_dots()
 # Q. geminata does not occur in these counties: Broward Co, FL; Glades Co, FL; Ware Co, FL; Hancock Co, MS
+
+tmap_mode("view")
+tm_shape(counties)+
+  tm_polygons()+
+  tm_shape(records[records$species == "Quercus margarettae" & records$source == "BISON",])+
+  tm_dots()
+# Q. margarettae does not occur in these counties: Leon Co, TX; Henderson Co, TX; Smith Co, TX; 
+
+## hold on a minute. BISON has added a bunch of county centroids that got past CoordinateCleaner. Are they all centroids??
+### no, not all BISON data are centroids, just a lot of them.
