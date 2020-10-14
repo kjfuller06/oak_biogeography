af5ae17bc7d9a771c348fd1f82abc78a50859256
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-10-14 12:44:57 -0500

    removed points within 100m of county centroids

--- scripts/all_data_join.R
+++ scripts/all_data_join.R
@@ -28,7 +28,33 @@ all = unique(all)
 map = all %>% 
   st_as_sf(coords = c(lon = "lon",
                       lat = "lat"),
-           crs = 4326)
+           crs = 4326) %>% 
+  st_transform(crs = 3083)
+
+# load counties for county centroid check
+counties <- st_read("data/county boundaries/acs_2012_2016_county_us_B27001.shp")
+counties = counties[-c(grep("Alaska", counties$NAME),grep("Hawaii", counties$NAME)),]
+counties = counties %>% 
+  st_transform(crs = 3083)
+# calculate the centroids and reproject to a projected crs for accurate distance calculations. Create a buffer of 100m around each centroid
+centroids = st_centroid(counties) %>% 
+  dplyr::select(NAME,
+                geometry) %>% 
+  st_buffer(dist = 100)
+
+# check if any records intersect with a 100m buffered county centroid layer
+map$ID = c(1:nrow(map))
+issues = st_join(centroids, map, left = FALSE)
+# tmap_mode("view")
+# tm_shape(counties)+tm_borders()+tm_shape(issues)+tm_dots()
+
+# remove issue points
+map = map[!c(map$ID %in% issues$ID),]
+all = map %>% 
+  st_transform(4326)
+all$lon = st_coordinates(all)[,1]
+all$lat = st_coordinates(all)[,2]
+all = st_set_geometry(all, NULL)
 
 # write to disk
 write.csv(all, "outputs/all_recordsV.1.csv", row.names = FALSE)
