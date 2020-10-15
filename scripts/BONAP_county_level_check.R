9b2f89781129fd1e8a9cfc1885d00b15f0026b83
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-10-15 15:13:33 -0500

    Removed all records occurring outside BONAP distributions

--- scripts/BONAP_county_level_check.R
+++ scripts/BONAP_county_level_check.R
@@ -4,117 +4,83 @@ library(tidyverse)
 library(raster)
 library(tmap)
 library(tmaptools)
-library(mapedit)
-library(mapview)
-
-# lf <- mapview()
-# 
-# # draw some polygons that we will select later
-# drawing <- lf %>%
-#   editMap()
-# 
-# # little easier now with sf
-# mapview(drawing$finished)
-# 
-# # especially easy with selectFeatures
-# selectFeatures(drawing$finished)
 
 # load layers
 counties <- st_read("outputs/counties.shp") %>% 
   dplyr::select(NAME,
                 geometry)
-
 records = read.csv("outputs/all_recordsV.1.csv")
 records = records %>% 
-  st_as_sf(coords = c(lon = "lon", lat = "lat"), crs = 4326) %>% 
-  st_transform(crs = 3083)
+  st_as_sf(coords = c(lon = "lon", lat = "lat"), crs = 4326)
 
+# remove all points that fall outside BONAP's distribution maps for each species
 # Q. chapmanii ####
-# generate county maps from manual copy of BONAP maps
-chap_cmn_bonap = selectFeatures(counties)
-chap_rare_bonap = selectFeatures(counties)
-chap_cmn_bonap$rarity = "common"
-chap_rare_bonap$rarity = "rare"
-chap_bonap = rbind(chap_cmn_bonap, chap_rare_bonap)
-write_sf(chap_bonap, "outputs/Qchap_bonap.shp")
-
-tmap_mode("view")
-tm_shape(counties)+
-  tm_polygons()+
-  tm_shape(records[records$species == "Quercus chapmanii",])+
-  tm_dots()
-# Q. chapmanii does not occur in these counties: Liberty Co, FL; Charlotte Co, FL; Glades Co, FL
+chap_map = read_sf("outputs/Qchap_bonap.shp")
+chap_rec = records[records$species == "Quercus chapmanii",]
+chap = chap_rec[chap_map, , join = st_intersects]
+# tm_shape(chap_map)+tm_borders()+tm_shape(chap)+tm_dots()
 
 # Q. geminata ####
-gem_cmn_bonap = selectFeatures(counties)
-# gem_rare_bonap = selectFeatures(counties)
-gem_cmn_bonap$rarity = "common"
-# gem_rare_bonap$rarity = "rare"
-gem_bonap = gem_cmn_bonap
-write_sf(gem_bonap, "outputs/Qgem_bonap.shp")
-
-tmap_mode("view")
-tm_shape(counties)+
-  tm_polygons()+
-  tm_shape(records[records$species == "Quercus geminata",])+
-  tm_dots()
-# Q. geminata does not occur in these counties: Broward Co, FL; Glades Co, FL; Ware Co, FL; Hancock Co, MS
+gem_map = read_sf("outputs/Qgem_bonap.shp")
+gem_rec = records[records$species == "Quercus geminata",]
+gem = gem_rec[gem_map, , join = st_intersects]
+# tm_shape(gem_map)+tm_borders()+tm_shape(gem)+tm_dots()
 
 # Q. hemisphaerica ####
-# generate county maps from manual copy of BONAP maps
-hem_cmn_bonap = selectFeatures(counties)
-hem_rare_bonap = selectFeatures(counties)
-hem_int_bonap = selectFeatures(counties)
-hem_cmn_bonap$rarity = "common"
-hem_rare_bonap$rarity = "rare"
-hem_int_bonap$rarity = "introduced"
-hem_bonap = rbind(hem_cmn_bonap, hem_rare_bonap)
-hem_bonap = rbind(hem_bonap, hem_int_bonap)
-write_sf(hem_bonap, "outputs/Qhem_bonap.shp")
+hem_map = read_sf("outputs/Qhem_bonap.shp")
+hem_rec = records[records$species == "Quercus hemisphaerica",]
+hem = hem_rec[hem_map, , join = st_intersects]
+# tm_shape(hem_map)+tm_borders()+tm_shape(hem)+tm_dots()
 
 # Q. margarettae ####
-# generate county maps from manual copy of BONAP maps
-mar_cmn_bonap = selectFeatures(counties)
-mar_rare_bonap = selectFeatures(counties)
-mar_cmn_bonap$rarity = "common"
-mar_rare_bonap$rarity = "rare"
-mar_bonap = rbind(mar_cmn_bonap, mar_rare_bonap)
-write_sf(mar_bonap, "outputs/Qmar_bonap.shp")
-
-tmap_mode("view")
-tm_shape(counties)+
-  tm_polygons()+
-  tm_shape(records[records$species == "Quercus margarettae" & records$source == "BISON",])+
-  tm_dots()
-# Q. margarettae does not occur in these counties: Leon Co, TX; Henderson Co, TX; Smith Co, TX; 
+mar_map = read_sf("outputs/Qmar_bonap.shp")
+mar_rec = records[records$species == "Quercus margarettae",]
+mar = mar_rec[mar_map, , join = st_intersects]
+# tm_shape(mar_map)+tm_borders()+tm_shape(mar)+tm_dots()
 
 # Q. michauxii ####
-# generate county maps from manual copy of BONAP maps
-mic_cmn_bonap = selectFeatures(counties)
-mic_rare_bonap = selectFeatures(counties)
-mic_cmn_bonap$rarity = "common"
-mic_rare_bonap$rarity = "rare"
-mic_bonap = rbind(mic_cmn_bonap, mic_rare_bonap)
-write_sf(mic_bonap, "outputs/Qmic_bonap.shp")
+mic_map = read_sf("outputs/Qmic_bonap.shp")
+mic_rec = records[records$species == "Quercus michauxii",]
+mic = mic_rec[mic_map, , join = st_intersects]
+# tm_shape(mic_map)+tm_borders()+tm_shape(mic)+tm_dots()
 
 # Q. phellos ####
-# generate county maps from manual copy of BONAP maps
-phe_cmn_bonap = selectFeatures(counties)
-phe_rare_bonap = selectFeatures(counties)
-phe_int_bonap = selectFeatures(counties)
-phe_cmn_bonap$rarity = "common"
-phe_rare_bonap$rarity = "rare"
-phe_int_bonap$rarity = "introduced"
-phe_bonap = rbind(phe_cmn_bonap, phe_rare_bonap)
-phe_bonap = rbind(phe_bonap, phe_int_bonap)
-write_sf(phe_bonap, "outputs/Qphe_bonap.shp")
+phe_map = read_sf("outputs/Qphe_bonap.shp")
+phe_rec = records[records$species == "Quercus phellos",]
+phe = phe_rec[phe_map, , join = st_intersects]
+# tm_shape(phe_map)+tm_borders()+tm_shape(phe)+tm_dots()
 
 # Q. shumardii ####
-# generate county maps from manual copy of BONAP maps
-shu_cmn_bonap = selectFeatures(counties)
-shu_rare_bonap = selectFeatures(counties)
-shu_cmn_bonap$rarity = "common"
-shu_rare_bonap$rarity = "rare"
-shu_bonap = rbind(shu_cmn_bonap, shu_rare_bonap)
-write_sf(shu_bonap, "outputs/Qshu_bonap.shp")
+shu_map = read_sf("outputs/Qshu_bonap.shp")
+shu_rec = records[records$species == "Quercus shumardii",]
+shu = shu_rec[shu_map, , join = st_intersects]
+# tm_shape(shu_map)+tm_borders()+tm_shape(shu)+tm_dots()
 
+# Q. stellata ####
+ste_map = read_sf("outputs/Qste_bonap.shp")
+ste_rec = records[records$species == "Quercus stellata",]
+ste = ste_rec[ste_map, , join = st_intersects]
+# tm_shape(ste_map)+tm_borders()+tm_shape(ste)+tm_dots()
+
+# Q. velutina ####
+vel_map = read_sf("outputs/Qvel_bonap.shp")
+vel_rec = records[records$species == "Quercus velutina",]
+vel = vel_rec[vel_map, , join = st_intersects]
+# tm_shape(vel_map)+tm_borders()+tm_shape(vel)+tm_dots()
+
+# Q. virginiana ####
+vir_map = read_sf("outputs/Qvir_bonap.shp")
+vir_rec = records[records$species == "Quercus virginiana",]
+vir = vir_rec[vir_map, , join = st_intersects]
+# tm_shape(vir_map)+tm_borders()+tm_shape(vir)+tm_dots()
+
+
+# write to disk ####
+all = rbind(chap, gem, hem, mar, mic, phe, shu, ste, vel, vir)
+all_nocoords = all
+all_nocoords$lon = st_coordinates(all)[,1]
+all_nocoords$lat = st_coordinates(all)[,2]
+st_geometry(all_nocoords) = NULL
+
+write_sf(all, "outputs/all_recordsV.1_bonapcleaned.shp")
+write.csv(all_nocoords, "outputs/all_recordsV.1_bonapcleaned.csv", row.names = FALSE)
