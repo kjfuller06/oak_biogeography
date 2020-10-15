d8141530a126926c023c728c4252e72353952fc0
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-10-15 16:35:11 -0500

    work in progress

--- scripts/raster_extract.R
+++ scripts/raster_extract.R
@@ -8,3 +8,8 @@ library(prism)
 # load dataset
 records = read_sf("outputs/all_recordsV.1_bonapcleaned.shp")
 
+# yrs = seq(from = 1990, to = 2019, by = 1)
+# options(prism.path = "data/prism data/ppt")
+# get_prism_annual('ppt', years = yrs, keepZip = TRUE)
+# precip_stack = prism_stack(ls_prism_data()[c(1:30),1])
+# ^ this doesn't work
