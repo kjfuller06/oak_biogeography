baa65daea6807547f51448c0da1d17e6304597fd
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-10-15 15:25:12 -0500

    starting over

--- scripts/raster_extract.R
+++ scripts/raster_extract.R
@@ -3,20 +3,8 @@ library(tidyverse)
 library(raster)
 library(lubridate)
 library(rnaturalearth)
+library(prism)
 
-# load Q. margarettiae dataset and extract values from WorldClim rasters
+# load dataset
+records = read_sf("outputs/all_recordsV.1_bonapcleaned.shp")
 
-# remove points outside the US
-# find ISO3 code for the US
-dplyr::filter(ccodes(), NAME %in% "United States")
-# load USA polygon
-usa = getData(name = "GADM", country = "USA", level = 1, download = TRUE) %>% 
-  st_as_sf() %>% 
-  dplyr::select(geometry)
-# remove stray points
-# records4 = st_join(records3, usa, join = st_within, left = FALSE)
-# # plot
-# ggplot(records4) +
-#   geom_sf(data = usa) +
-#   geom_sf(aes(color = SPCD))
-# ^ this takes too long to compute. Aborted.
