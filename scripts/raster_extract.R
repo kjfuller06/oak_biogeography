e4e8212b759a44bd6f90eca4616248a956e5a397
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-10-19 16:15:06 -0500

    Wrote some code to download data from NPN. Then realised I didn't have to.

--- scripts/raster_extract.R
+++ scripts/raster_extract.R
@@ -4,6 +4,8 @@ library(raster)
 library(lubridate)
 library(rnaturalearth)
 library(prism)
+library(rgdal)
+library(httr)
 
 # load dataset
 records = read_sf("outputs/all_recordsV.1_bonapcleaned.shp")
@@ -13,3 +15,9 @@ records = read_sf("outputs/all_recordsV.1_bonapcleaned.shp")
 # get_prism_annual('ppt', years = yrs, keepZip = TRUE)
 # precip_stack = prism_stack(ls_prism_data()[c(1:30),1])
 # ^ this doesn't work
+
+# for(i in c(1:365)){
+#   url <- paste('https://geoserver.usanpn.org/geoserver/wcs?service=WCS&version=2.0.1&request=GetCoverage&coverageId=gdd:30yr_avg_agdd&SUBSET=elevation(', i ,')&format=geotiff', sep = "")
+#   httr::GET(url,httr::write_disk(paste('data/NPN data/agdd', i, '.tif', sep = "")))
+# }
+
