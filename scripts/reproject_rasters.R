de3e76b578afd7e32272f3eca3caf0c1302f5e07
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-10-20 17:43:36 -0500

    Change to a means method rather than categorical method of resampling

--- scripts/reproject_rasters.R
+++ scripts/reproject_rasters.R
@@ -3,7 +3,7 @@ library(raster)
 # load the reference raster
 prsm_precip = raster("data/PRISM data/ppt/PRISM_ppt_30yr_normal_4kmM2_annual_asc.asc")
 
-# reproject CSGIARCSI and NPN datasets
+# # reproject CSGIARCSI and NPN datasets
 # aridity = raster('data/CGIARCSI data/ai_et0/ai_et0.tif')
 # PET = raster('data/CGIARCSI data/et0_yr/et0_yr.tif')
 # agdd = raster('data/NPN data/data.tif')
@@ -14,8 +14,8 @@ prsm_precip = raster("data/PRISM data/ppt/PRISM_ppt_30yr_normal_4kmM2_annual_asc
 # agdd = crop(agdd, extent(prsm_precip))
 # 
 # # reproject CSGIARCSI datasets
-# aridity = projectRaster(aridity, crs = "+proj=longlat +datum=NAD83 +no_defs")
-# PET = projectRaster(PET, crs = "+proj=longlat +datum=NAD83 +no_defs")
+# aridity = projectRaster(aridity, crs = "+proj=longlat +datum=NAD83 +no_defs", method = 'bilinear')
+# PET = projectRaster(PET, crs = "+proj=longlat +datum=NAD83 +no_defs", method = 'bilinear')
 # 
 # # write these to disk so I don't have to do it again
 # writeRaster(aridity, 'data/CGIARCSI data/ai_et0/at_et0_reprojected.tif', overwrite = TRUE)
@@ -28,13 +28,13 @@ PET = raster('data/CGIARCSI data/et0_yr/et0_yr_reprojected.tif')
 # resample to same extent and dimensions
 aridity = aggregate(aridity, fact = 5)
 PET = aggregate(PET, fact = 5)
-aridity <- resample(aridity, prsm_precip, method = 'ngb')
-PET <- resample(PET, prsm_precip, method = 'ngb')
+aridity <- resample(aridity, prsm_precip, method = 'bilinear')
+PET <- resample(PET, prsm_precip, method = 'bilinear')
 
 # write to disk
 writeRaster(aridity, 'data/CGIARCSI data/ai_et0/at_et0_cropped.tif', overwrite = TRUE)
 writeRaster(PET, 'data/CGIARCSI data/et0_yr/et0_yr_cropped.tif', overwrite = TRUE)
 
 # resample NPN dataset and write to disk
-agdd = resample(agdd, prsm_precip, method = 'ngb')
+agdd = resample(agdd, prsm_precip, method = 'bilinear')
 writeRaster(agdd, 'data/NPN data/agdd_reprojected.tif', overwrite = TRUE)
