964d1f62dd397cf4eb59f0f2738e8786cd8c332f
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-10-20 16:51:40 -0500

    Correcte errors with extent and resolution

--- scripts/reproject_rasters.R
+++ scripts/reproject_rasters.R
@@ -1,28 +1,40 @@
 library(raster)
 
-# load county data
-counties = read_sf("data/county boundaries/acs_2012_2016_county_us_B27001.shp") %>% 
-  st_transform(crs = 4269)
+# load the reference raster
+prsm_precip = raster("data/PRISM data/ppt/PRISM_ppt_30yr_normal_4kmM2_annual_asc.asc")
 
-# reproject CSGIARCSI datasets
-aridity = raster('data/CGIARCSI data/ai_et0/ai_et0.tif')
-aridity = crop(aridity, extent(-130, -50, 19, 54))
-aridity = projectRaster(aridity, crs = "+proj=longlat +datum=NAD83 +no_defs")
-writeRaster(aridity, 'data/CGIARCSI data/ai_et0/at_et0_reprojected.tif', overwrite = TRUE)
-aridity = crop(aridity, counties)
+# reproject CSGIARCSI and NPN datasets
+# aridity = raster('data/CGIARCSI data/ai_et0/ai_et0.tif')
+# PET = raster('data/CGIARCSI data/et0_yr/et0_yr.tif')
+# agdd = raster('data/NPN data/data.tif')
+# 
+# # crop these to match the prism datasets
+# aridity = crop(aridity, extent(prsm_precip))
+# PET = crop(PET, extent(prsm_precip))
+# agdd = crop(agdd, extent(prsm_precip))
+# 
+# # reproject CSGIARCSI datasets
+# aridity = projectRaster(aridity, crs = "+proj=longlat +datum=NAD83 +no_defs")
+# PET = projectRaster(PET, crs = "+proj=longlat +datum=NAD83 +no_defs")
+# 
+# # write these to disk so I don't have to do it again
+# writeRaster(aridity, 'data/CGIARCSI data/ai_et0/at_et0_reprojected.tif', overwrite = TRUE)
+# writeRaster(PET, 'data/CGIARCSI data/et0_yr/et0_yr_reprojected.tif', overwrite = TRUE)
+
+# reload datasets
+aridity = raster('data/CGIARCSI data/ai_et0/at_et0_reprojected.tif')
+PET = raster('data/CGIARCSI data/et0_yr/et0_yr_reprojected.tif')
+
+# resample to same extent and dimensions
 aridity = aggregate(aridity, fact = 5)
-writeRaster(aridity, 'data/CGIARCSI data/ai_et0/at_et0_cropped.tif', overwrite = TRUE)
-PET = raster('data/CGIARCSI data/et0_yr/et0_yr.tif')
-PET = crop(PET, extent(-130, -50, 19, 54))
-PET = projectRaster(PET, crs = "+proj=longlat +datum=NAD83 +no_defs")
-writeRaster(PET, 'data/CGIARCSI data/et0_yr/et0_yr_reprojected.tif', overwrite = TRUE)
-PET = crop(PET, counties)
 PET = aggregate(PET, fact = 5)
+aridity <- resample(aridity, prsm_precip, method = 'ngb')
+PET <- resample(PET, prsm_precip, method = 'ngb')
+
+# write to disk
+writeRaster(aridity, 'data/CGIARCSI data/ai_et0/at_et0_cropped.tif', overwrite = TRUE)
 writeRaster(PET, 'data/CGIARCSI data/et0_yr/et0_yr_cropped.tif', overwrite = TRUE)
 
 # resample NPN dataset and write to disk
-agdd = raster('data/NPN data/data.tif')
-agdd = crop(agdd, extent(-130, -50, 19, 54))
 agdd = resample(agdd, prsm_precip, method = 'ngb')
-agdd = crop(agdd, counties)
 writeRaster(agdd, 'data/NPN data/agdd_reprojected.tif', overwrite = TRUE)
