72937950a9eca79b556a66ac4316730191e22d0f
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-10-21 12:29:12 -0500

    Updated with new county records from BONAP

--- scripts/BONAP_records_cleaning.R
+++ scripts/BONAP_records_cleaning.R
@@ -8,71 +8,101 @@ library(tmaptools)
 # load layers
 counties <- st_read("outputs/counties.shp") %>% 
   dplyr::select(NAME,
-                geometry)
+                geometry) %>% 
+  st_transform(crs = 4326)
+# co = unique(counties$NAME)
+# write.csv(co, "data/censuscounties.csv")
+co = read.csv("data/censuscounties.csv")
+bonap = read.csv("data/BONAP range maps/BONAP.csv")
+bonap = bonap %>% 
+  left_join(co[,c(1,3,5)], by = c("state2" = "State",
+                       "County" = "Short"))
+
 records = read.csv("outputs/all_recordsV.1.csv")
 records = records %>% 
   st_as_sf(coords = c(lon = "lon", lat = "lat"), crs = 4326)
 
 # remove all points that fall outside BONAP's distribution maps for each species
 # Q. chapmanii ####
-chap_map = read_sf("outputs/Qchap_bonap.shp")
+chap_counties = bonap %>% 
+  filter(Scientific.Name == "Quercus chapmanii")
+chap_map = counties %>% 
+  filter(NAME %in% chap_counties$All)
 chap_rec = records[records$species == "Quercus chapmanii",]
 chap = chap_rec[chap_map, , join = st_intersects]
-# tm_shape(chap_map)+tm_borders()+tm_shape(chap)+tm_dots()
+
 
 # Q. geminata ####
-gem_map = read_sf("outputs/Qgem_bonap.shp")
+gem_counties = bonap %>% 
+  filter(Scientific.Name == "Quercus geminata")
+gem_map = counties %>% 
+  filter(NAME %in% gem_counties$All)
 gem_rec = records[records$species == "Quercus geminata",]
 gem = gem_rec[gem_map, , join = st_intersects]
-# tm_shape(gem_map)+tm_borders()+tm_shape(gem)+tm_dots()
 
 # Q. hemisphaerica ####
-hem_map = read_sf("outputs/Qhem_bonap.shp")
+hem_counties = bonap %>% 
+  filter(Scientific.Name == "Quercus hemisphaerica")
+hem_map = counties %>% 
+  filter(NAME %in% hem_counties$All)
 hem_rec = records[records$species == "Quercus hemisphaerica",]
 hem = hem_rec[hem_map, , join = st_intersects]
-# tm_shape(hem_map)+tm_borders()+tm_shape(hem)+tm_dots()
 
 # Q. margarettae ####
-mar_map = read_sf("outputs/Qmar_bonap.shp")
+mar_counties = bonap %>% 
+  filter(Scientific.Name == "Quercus margarettae")
+mar_map = counties %>% 
+  filter(NAME %in% mar_counties$All)
 mar_rec = records[records$species == "Quercus margarettae",]
 mar = mar_rec[mar_map, , join = st_intersects]
-# tm_shape(mar_map)+tm_borders()+tm_shape(mar)+tm_dots()
 
 # Q. michauxii ####
-mic_map = read_sf("outputs/Qmic_bonap.shp")
+mic_counties = bonap %>% 
+  filter(Scientific.Name == "Quercus michauxii")
+mic_map = counties %>% 
+  filter(NAME %in% mic_counties$All)
 mic_rec = records[records$species == "Quercus michauxii",]
 mic = mic_rec[mic_map, , join = st_intersects]
-# tm_shape(mic_map)+tm_borders()+tm_shape(mic)+tm_dots()
 
 # Q. phellos ####
-phe_map = read_sf("outputs/Qphe_bonap.shp")
+phe_counties = bonap %>% 
+  filter(Scientific.Name == "Quercus phellos")
+phe_map = counties %>% 
+  filter(NAME %in% phe_counties$All)
 phe_rec = records[records$species == "Quercus phellos",]
 phe = phe_rec[phe_map, , join = st_intersects]
-# tm_shape(phe_map)+tm_borders()+tm_shape(phe)+tm_dots()
 
 # Q. shumardii ####
-shu_map = read_sf("outputs/Qshu_bonap.shp")
+shu_counties = bonap %>% 
+  filter(Scientific.Name == "Quercus shumardii")
+shu_map = counties %>% 
+  filter(NAME %in% shu_counties$All)
 shu_rec = records[records$species == "Quercus shumardii",]
 shu = shu_rec[shu_map, , join = st_intersects]
-# tm_shape(shu_map)+tm_borders()+tm_shape(shu)+tm_dots()
 
 # Q. stellata ####
-ste_map = read_sf("outputs/Qste_bonap.shp")
+ste_counties = bonap %>% 
+  filter(Scientific.Name == "Quercus stellata")
+ste_map = counties %>% 
+  filter(NAME %in% ste_counties$All)
 ste_rec = records[records$species == "Quercus stellata",]
 ste = ste_rec[ste_map, , join = st_intersects]
-# tm_shape(ste_map)+tm_borders()+tm_shape(ste)+tm_dots()
 
 # Q. velutina ####
-vel_map = read_sf("outputs/Qvel_bonap.shp")
+vel_counties = bonap %>% 
+  filter(Scientific.Name == "Quercus velutina")
+vel_map = counties %>% 
+  filter(NAME %in% vel_counties$All)
 vel_rec = records[records$species == "Quercus velutina",]
 vel = vel_rec[vel_map, , join = st_intersects]
-# tm_shape(vel_map)+tm_borders()+tm_shape(vel)+tm_dots()
 
 # Q. virginiana ####
-vir_map = read_sf("outputs/Qvir_bonap.shp")
+vir_counties = bonap %>% 
+  filter(Scientific.Name == "Quercus virginiana")
+vir_map = counties %>% 
+  filter(NAME %in% vir_counties$All)
 vir_rec = records[records$species == "Quercus virginiana",]
 vir = vir_rec[vir_map, , join = st_intersects]
-# tm_shape(vir_map)+tm_borders()+tm_shape(vir)+tm_dots()
 
 
 # write to disk ####
